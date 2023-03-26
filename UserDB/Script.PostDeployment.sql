0eb2d584dd5cd67564df2e3bc43f56a58d7e607e
Steve McTeague <stevemcteague@gmail.com>
2023-03-26 19:11:49 -0400

    Initial setup

---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,8 @@
+﻿if not exists (select 1 from dbo.[User])
+begin
+	insert into dbo.[User] (FirstName, LastName)
+	values ('Steve', 'McTeague'),
+	('Nick', 'Lemke'),
+	('Bob', 'McNulty'),
+	('Tabitha', 'Schiedel');
+end
