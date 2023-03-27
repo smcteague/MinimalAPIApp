d754629cec84ec48c53f89ef97cc01c941b6aecf
Steve McTeague <stevemcteague@gmail.com>
2023-03-27 15:19:27 -0400

    Implementing code behind.

---
+++ WebApp/Pages/FetchUserData.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace WebApp.Pages;
+
+public class FetchUserDataBase : ComponentBase
+{
+    protected List<Models.UserModel> UsersList { get; set; } = new List<Models.UserModel>();
+
+    protected async override Task OnInitializedAsync()
+    {
+        var httpClient = new HttpClient();
+        UsersList = await httpClient.GetFromJsonAsync<List<Models.UserModel>>("https://localhost:7040/users");
+
+        base.OnInitialized();
+    }
+}
+
+
