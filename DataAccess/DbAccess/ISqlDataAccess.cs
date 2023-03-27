0e5fad782a76d5f86b8f9468689ec1092c84de7b
Steve McTeague <stevemcteague@gmail.com>
2023-03-26 20:45:25 -0400

    formatting

--- DataAccess/DbAccess/ISqlDataAccess.cs
+++ DataAccess/DbAccess/ISqlDataAccess.cs
@@ -1,8 +1,7 @@
-﻿namespace DataAccess.DbAccess
+﻿namespace DataAccess.DbAccess;
+
+public interface ISqlDataAccess
 {
-    public interface ISqlDataAccess
-    {
-        Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
-        Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Default");
-    }
+    Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Default");
+    Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Default");
 }
